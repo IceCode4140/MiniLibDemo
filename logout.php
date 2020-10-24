ec383529a4b818dd68b69c5ffcd705578a3b5f96
IceCode4140 <73382016+IceCode4140@users.noreply.github.com>
2020-10-24 20:16:32 +0900

    Fix logout.

--- logout.php
+++ logout.php
@@ -1,15 +1,7 @@
-<html>				
-<head>							
-    <meta http-equiv="Content-type" content="text/html;charset=UTF-8">	
-    <title>Online Library E-books</title>
-</head>
-<body>
 <?php
     session_start();
     unset($_SESSION["username"]);
     header("location:home_index.html");
 ?>
-</body>
-</html>
 
 
