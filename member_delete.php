58eb41c3e2c3584feb90e3347860b19b01d4e368
H. Abe <73382016+IceCode4140@users.noreply.github.com>
2020-10-25 17:17:21 +0900

    fix member bug.

--- member_delete.php
+++ member_delete.php
@@ -1,10 +1,3 @@
-<!DOCTYPE html>
-<html>				
-<head>							
-    <meta http-equiv="Content-type" content="text/html;charset=UTF-8">	
-    <title>Member Delete Action</title>
-</head>
-<body>
 <?php  
     session_start();
     require_once("connect.php");
@@ -13,11 +6,8 @@
     require_once("memberlogin_check.php");
 
     $sql = "DELETE FROM members WHERE username = '{$_SESSION['username']}'"; 
-    echo $sql;
     mysqli_query($db,$sql) or  die (mysqli_error($db));
     
     unset($_SESSION["username"]);
     header("location:home_index.html"); 
 ?>
-</body>
-</html>
